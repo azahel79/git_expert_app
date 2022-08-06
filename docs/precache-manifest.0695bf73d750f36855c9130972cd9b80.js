dbfe5363df4580679a1208b993f69e1ff4ab7476
azahel79 <azahelprogramer79@gmail.com>
2022-08-06 01:12:13 -0500

    deploy git pages

---
+++ docs/precache-manifest.0695bf73d750f36855c9130972cd9b80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed8fa4da40f87b320f02",
+    "url": "/static/css/main.69094683.chunk.css"
+  },
+  {
+    "revision": "ed8fa4da40f87b320f02",
+    "url": "/static/js/main.ed8fa4da.chunk.js"
+  },
+  {
+    "revision": "8ab1872ef407a50de177",
+    "url": "/static/js/1.8ab1872e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f288c58562ff83dd0b79b9e5f0279cc5",
+    "url": "/index.html"
+  }
+];
